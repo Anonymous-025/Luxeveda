2f9ee391205aaee6b85c8ca4000fc65507df6111
Anonymous-025 <unknownjesvin>
2024-09-16 16:07:51 +0530

    new  one

--- client/README.md
+++ client/README.md
@@ -1,5 +1,21 @@
-# Vue 3 + TypeScript + Vite
+# vue-cart
 
-This template should help get you started developing with Vue 3 and TypeScript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+Shopping cart example using Vue 3, Vite, [daisyUI](https://daisyui.com/) and [Pinia](https://pinia.esm.dev/).
 
-Learn more about the recommended Project Setup and IDE Support in the [Vue Docs TypeScript Guide](https://vuejs.org/guide/typescript/overview.html#project-setup).
+Demo: https://vue-cart.vercel.app
+
+## Usage
+
+```
+# Install dependencies
+$ npm install
+
+# Compiles and hot-reloads for development
+$ npm run dev
+
+# Compiles and minifies for production
+$ npm run build
+
+# Vite preview
+$ npm run serve
+```
--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,41 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { computed } from 'vue'
+import Nav from './components/Nav.vue'
+import { usePersistCart } from './composables/usePersistCart'
+import { useCartStore } from './store/cart'
+import { useProductStore } from './store/products'
+
+const productStore = useProductStore()
+const cartStore = useCartStore()
+
+productStore.fetchAll()
+usePersistCart()
+
+const count = computed(() => cartStore.count)
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="drawer">
+    <input id="drawer-input" type="checkbox" class="drawer-toggle">
+    <div class="bg-base-100 text-base-content min-h-screen drawer-content">
+      <Nav />
+      <router-view />
+    </div>
+    <div class="drawer-side">
+      <label for="drawer-input" class="drawer-overlay" />
+      <ul class="p-4 overflow-y-auto menu w-80 bg-base-100 text-base-content">
+        <li>
+          <router-link to="/">
+            Home
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/cart">
+            Cart
+            <div class="badge ml-2 badge-outline" v-text="count" />
+          </router-link>
+        </li>
+      </ul>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
